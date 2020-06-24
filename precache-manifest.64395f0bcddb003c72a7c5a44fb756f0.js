a2e2cc23f03f4ed3f18b5b962e353f3e1a1b3a7e
Teemu Koivumaa <teemu.koivumaa@nokia.com>
2020-06-24 12:27:41 +0300

    Updates

---
+++ precache-manifest.64395f0bcddb003c72a7c5a44fb756f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3aafe644995998700ac702af6939bd",
+    "url": "/colory/index.html"
+  },
+  {
+    "revision": "ad9382f40d968399fc80",
+    "url": "/colory/static/css/main.109600a9.chunk.css"
+  },
+  {
+    "revision": "d76b64c9195745bc319b",
+    "url": "/colory/static/js/2.657e3f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colory/static/js/2.657e3f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9382f40d968399fc80",
+    "url": "/colory/static/js/main.402621bf.chunk.js"
+  },
+  {
+    "revision": "a2cd0055a9a3ae0fc82e",
+    "url": "/colory/static/js/runtime-main.1f6fc5b4.js"
+  }
+]);
